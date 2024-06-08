364d1564b4dd9a532a03df63bfcdab03965e74b1
koyhoio <f_nyu27@yahoo.co.jp>
2024-06-08 23:03:27 +0900

    課題の提出

---
+++ src/animal/Human.java
@@ -0,0 +1,32 @@
+package animal;
+
+public class Human extends Animal implements Thinkable{
+    private String hobby;
+
+
+
+    public String getHobby() {
+        return hobby;
+    }
+
+
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Human() {
+
+
+    }
+
+    public Human(String name, int age) {
+        super(name,age);
+    }
+
+    public void think() {
+        System.out.println("私は"+hobby+"について考えています。");
+
+    }
+
+}
